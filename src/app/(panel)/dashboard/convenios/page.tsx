516c59d139410fc8b9811be20028d5a252970255
HerbertonMiguel-Dev <herbertonmiguel.dev@gmail.com>
2025-10-30 23:11:43 -0300

    feat(convenios): adiciona page.tsx com ConveniosContent e título da página

--- src/app/(panel)/dashboard/convenios/page.tsx
+++ src/app/(panel)/dashboard/convenios/page.tsx
@@ -1,8 +1,15 @@
 
+import { ConveniosContent } from "./_components/convenios-content";
+
 export default function Convenios() {
   return (
     <section>
-      <h1>Página Convênios</h1>
+     
+     
+      <ConveniosContent />
     </section>
   )
 }
+
+
+
