1a8faa7053578ba32b5ca50c4068701045c6d204
HerbertonMiguel-Dev <herbertonmiguel.dev@gmail.com>
2025-11-02 12:01:34 -0300

    feat: criar componente ServicosContent para buscar e renderizar a lista de servicos

--- src/app/(panel)/dashboard/servicos/_components/servicos-content.tsx
+++ src/app/(panel)/dashboard/servicos/_components/servicos-content.tsx
@@ -1,22 +1,14 @@
-//src/app/(panel)/servicos/_components/servicos-content.tsx
+//src/app/(panel)/dashboard/servicos/_components/servicos-content.tsx
 
-import { getAllServicos } from "../_data-access/get-all-servicos"
-import { ServicosList } from "./servicos-list"
+import { getAllServicos } from '../_data-access/get-all-servicos'
+import { ServicosList } from './servicos-list'
 
 export async function ServicosContent() {
-  const servicos = await getAllServicos()
-
+  const services = await getAllServicos()
+  
   return (
-    <section className="mx-auto">
-      <h2 className="text-2xl font-bold mb-4">Serviços disponíveis</h2>
-
-      {servicos.error && (
-        <p className="text-red-500">{servicos.error}</p>
-      )}
-
-      {!servicos.error && (
-        <ServicosList servicos={servicos.data || []} />
-      )}
+    <section>
+      <ServicosList services={services.data || []} />
     </section>
   )
 }
